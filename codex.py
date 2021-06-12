b867b49203180e8b2295cc6bbde07b76bf7f39d4
khushmeeet <khushmeetsingh1996@gmail.com>
2021-06-13 00:02:58 +0530

    Integrated mongoengine with flask

--- codex.py
+++ codex.py
@@ -2,10 +2,20 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-db = MongoEngine(app)
 app.config.from_json("config.json")
+db = MongoEngine(app)
+
+
+class Codex(db.Document):
+    name = db.StringField(required=True, unique=True)
 
 
 @app.route('/')
 def hello():
     return 'hello'
+
+
+@app.route('/s')
+def save():
+    m = Codex(name='janice').save()
+    return m.name
