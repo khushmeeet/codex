7264f694cc787a308661d512ad7f673672fe19af
khushmeeet <khushmeetsingh1996@gmail.com>
2021-06-13 01:43:32 +0530

    Bare min of codex working

--- codex.py
+++ codex.py
@@ -1,21 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mongoengine import MongoEngine
+import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_json("config.json")
 db = MongoEngine(app)
 
 
 class Codex(db.Document):
-    name = db.StringField(required=True, unique=True)
+    content = db.StringField(required=True)
+    added_on = db.DateTimeField(required=True)
+    source = db.StringField(required=True)
+    source_type = db.StringField(required=True)
 
 
 @app.route('/')
-def hello():
-    return 'hello'
+def get_all_documents():
+    all_documents = Codex.objects
+    return render_template('index.html', documents=all_documents)
 
 
-@app.route('/s')
-def save():
-    m = Codex(name='janice').save()
-    return m.name
+@app.route('/new', methods=['POST'])
+def new_document():
+    content = request.form['content']
+    source = request.form['source']
+    source_type = request.form['source_type']
+    c = Codex(content=content, added_on=datetime.datetime.now(), source=source, source_type=source_type).save()
+    return redirect(url_for('get_all_documents'))
