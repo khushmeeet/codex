26c79e32e5ca7bf0341ab5211e5282a47ef026e3
khushmeeet <khushmeetsingh1996@gmail.com>
2021-06-16 02:08:25 +0530

    Working forms input with sqlite db

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from .models import Notes
 
 
 class NoteForm(forms.Form):
-    content = forms.Textarea()
+    content = forms.CharField(widget=forms.Textarea, required=True)
     source = forms.CharField(label='Where is the note taken from?', required=True, max_length=500)
     source_type = forms.ChoiceField(label='What is the type of the source?', required=True, choices=Notes.SOURCE_TYPE_CHOICES)
     url = forms.URLField(label='URL of the source, if any', required=False)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Notes
 from main.forms import NoteForm
 
@@ -16,10 +16,18 @@ def new_note(request):
     if request.method == 'POST':
         form = NoteForm(request.POST)
         if form.is_valid():
-            return HttpResponse('Got the form values')
+            note = Notes(
+                content=form.cleaned_data['content'],
+                source=form.cleaned_data['source'],
+                url=form.cleaned_data['url'],
+                added_on=form.cleaned_data['added_on'],
+                source_type=form.cleaned_data['source_type']
+            )
+            note.save()
+            return redirect('notes_list')
     else:
         form = NoteForm()
         context = {
             'form': form
         }
-        return render(request, 'home.html', context)
+        return render(request, 'new-note.html', context)
